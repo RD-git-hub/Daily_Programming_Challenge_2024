36c6c0d59fe086151047ec13a5810070a60e8ff9
RD-git-hub <150375525+RD-git-hub@users.noreply.github.com>
2024-09-24 21:48:21 +0530

    Add files via upload

---
+++ Day16GCD.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Day16GCD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int gcd = findGCD(num1, num2);
+        int lcm = (num1 * num2) / gcd;
+        System.out.println(lcm);
+    }
+
+    public static int findGCD(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+        return num1;
+    }
+}
