858f8eda56bc40b4bf01440dba8a7afb361d8b23
RD-git-hub <150375525+RD-git-hub@users.noreply.github.com>
2024-09-28 23:16:16 +0530

    Add files via upload

---
+++ Day20SortStack.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class Day20SortStack {
+    public static void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int temp = stack.pop();
+            sortStack(stack);
+            insertInSortedOrder(stack, temp);
+        }
+    }
+
+    private static void insertInSortedOrder(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || element > stack.peek()) {
+            stack.push(element);
+        } else {
+            int temp = stack.pop();
+            insertInSortedOrder(stack, element);
+            stack.push(temp);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(1);
+        stack.push(4);
+        stack.push(2);
+        sortStack(stack);
+        Stack<Integer> sortedStack = new Stack<>();
+        while (!stack.isEmpty()) {
+            sortedStack.push(stack.pop());
+        }
+        while (!sortedStack.isEmpty()) {
+            System.out.print(sortedStack.pop() + " ");
+        }
+    }
+}
